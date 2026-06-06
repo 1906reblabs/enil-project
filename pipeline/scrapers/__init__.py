43d5ba9cb441c19c2c61950de7f2b6f6c12789a8
1906reblabs <1906reblabs@gmail.com>
2026-06-06 10:23:37 +0200

    File created

--- pipeline/scrapers/__init__.py
+++ pipeline/scrapers/__init__.py
@@ -1,1 +1,8 @@
+# ENIL Scrapers Package
+# Place this file at: enil-data/pipeline/scrapers/__init__.py
+from .dbe_scraper import DBEScraper
+from .nsfas_monitor import NSFASMonitor
+from .aps_scraper import APSScraper
+from .labour_scraper import LabourScraper
 
+__all__ = ["DBEScraper", "NSFASMonitor", "APSScraper", "LabourScraper"]
